59270cd820a4d6b433c0423a551dc4d6e8c43e87
bagel55 <93285852+bagel55@users.noreply.github.com>
2024-08-04 17:57:45 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,4 @@
 { config, pkgs, ... }:
-
 {
   imports =
     [ 
@@ -8,26 +7,26 @@
       ./pkg-exclusions.nix
       ./pkg-inclusions.nix
     ];
-    
+  
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true;
   
   system.autoUpgrade.channel = "https://channels.nixos.org/nixos-unstable";
   nixpkgs.config.channel = "https://channels.nixos.org/nixos-unstable";
-
+  
   # Bootloader.
   boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
   boot.loader.systemd-boot.enable = true;
-
+  
   # Enable networking
   networking.networkmanager.enable = true;
-
+  
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
-
+  
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
+  
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -39,7 +38,7 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
-
+  
   # Configure keymap in X11
   services.xserver = {
     xkb.layout = "us";
@@ -48,20 +47,20 @@
   
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
+  
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
+  
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
+  
   # Enable lvm2
   services.lvm.enable = true;
-
+  
   # Enable flatpak
   services.flatpak.enable = true;
-
+  
   # Enable sound with pipewire.
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
@@ -74,35 +73,35 @@
     pulse.enable = true; 
     jack.enable = true;
   };
-
+  
   users.users.bagel = {
     isNormalUser = true;
     description = "bagel";
     extraGroups = [ "networkmanager" "wheel" "dialout" "input" ];
     packages = with pkgs; [];
   };
-
+  
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "bagel";
-
+  
   # Workaround for GNOME autologin
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
-
+  
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
+  
   fonts.packages = with pkgs; [
     nerdfonts
   ];
-
+  
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
-
+  
   system.stateVersion = "23.11";
 }
