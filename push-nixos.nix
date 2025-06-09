7dbb62a44d6f8261dbf6c2e78307b0da7401d65f
bagel <bagel2255@protonmail.com>
2025-06-08 23:47:27 -0700

    Auto backup on 2025-06-08 23:47:27

--- push-nixos.nix
+++ push-nixos.nix
@@ -9,5 +9,20 @@ in {
       ${pushScript}/bin/push-nixos
     '';
   };
+  
+  systemd.services.nixos-git-pull = {
+    description = "Pull latest NixOS config on boot";
+    wantedBy = [ "multi-user.target" ];  # Run at boot after basic system is up
+    after = [ "network-online.target" ]; # Wait until network is ready
+    requires = [ "network-online.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.git}/bin/git -C /etc/nixos pull";
+      # Optional: Set up correct SSH environment if needed
+      #Environment = "GIT_SSH_COMMAND=ssh -i /root/.ssh/id_ed25519 -o IdentitiesOnly=yes";
+      User = "root";  # Default, but explicit
+    };
+  };
 }
 
