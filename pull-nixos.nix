6112f502ebd9fd35663c899022af90f9a3258a4a
bagel <bagel2255@protonmail.com>
2025-06-09 08:35:32 -0700

    Auto backup on 2025-06-09 08:35:32

--- pull-nixos.nix
+++ pull-nixos.nix
@@ -18,20 +18,21 @@ in {
   set -e
 
   LOCKFILE="/var/lock/nixos-git-pull.lock"
+  LOGFILE="/home/bagel/nixos-git-pull.log"
 
   # Attempt to acquire lock
   exec 9>"$LOCKFILE"
   if ! flock -n 9; then
-    echo "[INFO] Another rebuild is in progress. Exiting."
+    echo "[INFO] Another rebuild is in progress. Exiting." >> "$LOGFILE" 2>&1
     exit 0
   fi
 
   cd /etc/nixos
-  echo "[INFO] Fetching latest changes from origin..."
-  ${gitPath} fetch origin main
-  echo "[INFO] Pulling latest changes and rebuilding NixOS..."
-  ${gitPath} reset --hard origin/main
-  ${nixosRebuild} switch --upgrade
+  echo "[INFO] Fetching latest changes from origin..." >> "$LOGFILE" 2>&1
+  ${gitPath} fetch origin main >> "$LOGFILE" 2>&1
+  echo "[INFO] Pulling latest changes and rebuilding NixOS..." >> "$LOGFILE" 2>&1
+  ${gitPath} reset --hard origin/main >> "$LOGFILE" 2>&1
+  ${nixosRebuild} switch --upgrade >> "$LOGFILE" 2>&1
 '';
       User = "root";
     };
