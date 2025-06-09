9d36231834f312a903c1257b2c30fdca27c7cf62
bagel <bagel2255@protonmail.com>
2025-06-09 00:42:17 -0700

    Auto backup on 2025-06-09 00:42:17

--- pull-nixos.nix
+++ pull-nixos.nix
@@ -13,16 +13,13 @@ in {
 
     serviceConfig = {
       Type = "oneshot";
-      Environment = "GIT_SSH_COMMAND=${sshPath} -i /root/.ssh/id_ed25519 -o IdentitiesOnly=yes NIX_PATH=/home/bagel/.nix-defexpr/channels:nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels";
+      Environment = "GIT_SSH_COMMAND=${sshPath} -i /root/.ssh/id_ed25519 -o IdentitiesOnly=yes";
       ExecStart = pkgs.writeShellScript "git-pull-nixos-and-rebuild" ''
         set -e
-        exec 9>/var/lock/nixos-rebuild.lock
-        flock -n 9 || { echo "[ERROR] Another nixos-rebuild is already running. Exiting."; exit 1; }
         cd /etc/nixos
         echo "[INFO] Pulling latest changes and rebuilding NixOS..."
         ${gitPath} pull origin main
-        ${pkgs.nix}/bin/nix-channel --update
-        ${nixosRebuild} switch --upgrade
+        ${nixosRebuild} switch
       '';
       User = "root";
     };
