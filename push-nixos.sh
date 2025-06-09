2076fa4cd11c56f916e96867fba101c2c2b6da92
bagel <bagel2255@protonmail.com>
2025-06-08 23:27:51 -0700

    Auto backup on 2025-06-08 23:27:51

--- push-nixos.sh
+++ push-nixos.sh
@@ -8,5 +8,7 @@ git config user.email "bagel2255@protonmail.com"
 
 git add .
 git commit -m "Auto backup on $(date '+%Y-%m-%d %H:%M:%S')" || true
+
+export GIT_SSH_COMMAND='ssh -i /root/.ssh/id_ed25519 -o IdentitiesOnly=yes'
 git push origin main
 
