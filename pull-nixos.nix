6e2a1a14223ac4f1b7ad0b3a723f20c6b95216af
bagel <bagel2255@protonmail.com>
2025-06-09 09:56:36 -0700

    Auto backup on 2025-06-09 09:56:36

--- pull-nixos.nix
+++ pull-nixos.nix
@@ -16,19 +16,12 @@ in {
       GIT_SSH_COMMAND=${sshPath} -i /root/.ssh/id_ed25519 -o IdentitiesOnly=yes
       PATH=/run/current-system/sw/bin:/usr/bin:/bin
     '';
-    ExecStartPre = "${pkgs.coreutils}/bin/sleep 15";
     ExecStart = pkgs.writeShellScript "git-pull-on-boot" ''
       set -e
       LOGFILE="/home/bagel/git-pull-on-boot.log"
 
       echo "[INFO] Starting git fetch + pull at $(date)" >> "$LOGFILE" 2>&1
       cd /etc/nixos
-      
-	echo "RESOLV CONF:"
-	cat /etc/resolv.conf >> "$LOGFILE" 2>&1
-	echo "PING TEST:"
-	ping -c 3 github.com >> "$LOGFILE" 2>&1 || echo "Ping failed" >> "$LOGFILE" 2>&1
-
 
       ${gitPath} fetch origin main >> "$LOGFILE" 2>&1
       ${gitPath} reset --hard origin/main >> "$LOGFILE" 2>&1
