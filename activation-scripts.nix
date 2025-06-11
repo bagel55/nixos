f2b7035b09d2a0e30589222c57e62d69feb2b9a4
bagel <bagel2255@protonmail.com>
2025-06-11 08:14:01 -0700

    Auto backup on 2025-06-11 08:14:01

---
+++ activation-scripts.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+system.activationScripts.nixos-permissions = {
+  text = ''
+    echo "[Permissions] Setting /etc/nixos group to wheel with write permissions"
+
+    chown -R root:wheel /etc/nixos
+    chmod -R g+rw /etc/nixos
+    find /etc/nixos -type d -exec chmod g+s {} \;
+  '';
+};
