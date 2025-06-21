1fb6c108294d3c263bace597c5447d1719bf6256
bagel <bagel2255@protonmail.com>
2025-06-21 16:32:19 -0700

    Auto backup on 2025-06-21 16:32:19

--- configuration.nix
+++ configuration.nix
@@ -39,12 +39,11 @@
     	  xkb.variant = "";
   	};
 
-  	#Enable the X11 windowing system.
-  	services.xserver.enable = true;
-
   	#Enable the GNOME Desktop Environment.
-  	services.displayManager.gdm.enable = true;
-  	services.desktopManager.gnome.enable = true;
+  	services.xserver.enable = true;
+  	services.xserver.desktopManager.gnome.enable = true;
+  	services.xserver.displayManager.gdm.enable = true;
+	services.xserver.displayManager.gdm.wayland = true;
   	services.gnome.core-apps.enable = false;
   	services.xserver.excludePackages = [ pkgs.xterm ];
   	environment.gnome.excludePackages = with pkgs; [pkgs.gnome-tour];
