09f6baa7d0bd2899a82c42be33f022e5c51d9297
bagel <bagel2255@protonmail.com>
2025-06-24 10:24:04 -0700

    Auto backup on 2025-06-24 10:24:04

--- pkg-inclusions.nix
+++ pkg-inclusions.nix
@@ -3,86 +3,88 @@
   unstableSmall = import <nixos-unstable-small> { config = {}; };
 in
 {environment.systemPackages = with pkgs; [
+
 # Gnome
-gnome-shell-extensions #Gnome Extensions
-gnome-tweaks #Tweaks
-gnome-themes-extra
-gnome-disk-utility #Disk Tool
-alacarte #Gnome Menu Editor
-baobab #Disk Analyser
-gnome-calculator #Calculator
-nautilus #File Explorer
-gnome-text-editor #Text Editor GUI App
-eog #Images
-totem #Videos
-evince #Documents
+gnome-shell-extensions #Gnome extensions.
+gnome-tweaks #Tweaks.
+gnome-themes-extra #More themes to add propper dark mode.
+gnome-disk-utility #Disk tool.
+alacarte #Gnome menu editor.
+baobab #Disk analyser.
+gnome-calculator #Calculator.
+nautilus #File explorer.
+gnome-text-editor #Text editor GUI app.
+eog #Images.
+totem #Videos.
+evince #Documents.
 
 # Util
-btop #System Monitor
-nvtopPackages.full #GPU Monitor
-corectrl #GPU Configuration
-openrgb-with-all-plugins #RGB Control
-pavucontrol #Audio Devices Configuration
-helvum #Audio Porting
-fastfetch #Loonix Redditing
-obsidian #Notes
+btop #System monitor.
+nvtopPackages.full #GPU monitor.
+corectrl #GPU configuration.
+openrgb-with-all-plugins #RGB control.
+pavucontrol #Audio devices configuration.
+helvum #Audio porting.
+fastfetch #Loonix redditing.
+obsidian #Notes.
 
 # Wine and friends
-wine64Packages.stagingFull #Wine 64 Bit Tools
-winePackages.stagingFull #Wine 32 Bit Tools
-winetricks #Wine Prefix Editor
-protontricks #Proton Prefix Editor
-proton-caller #Proton Updater
-protonup #Proton GE
-protonup-qt #Proton For Lutris
+wine64Packages.stagingFull #Wine 64 bit tools.
+winePackages.stagingFull #Wine 32 bit tools.
+winetricks #Wine prefix editor.
+protontricks #Proton prefix editor.
+proton-caller #Proton updater.
+protonup #Proton GE.
+protonup-qt #Proton for easy apply to other apps.
 
 # Basic Applications
-alacritty #Terminal Emulator
-brave #Web Browser
-discord #Discord
-spotify #Spotify
+alacritty #Terminal emulator.
+brave #Web browser.
+discord #Discord.
+spotify #Spotify.
 
 # Games
-lutris #The OG
-heroic-unwrapped #Epic and GOG
-xivlauncher #FFXIV Dalamud Launcher
-prismlauncher #Minecraft Launcher
-r2modman #Mod Stuffs For Loads of Games
-fflogs
-gamemode
+lutris #The OG.
+heroic-unwrapped #Epic and GOG.
+xivlauncher #FFXIV dalamud launcher.
+prismlauncher #Minecraft launcher.
+r2modman #Mod stuffs for loads of games.
+fflogs #Not ego parsing.
+gamemode #Optimizations for games.
 
 # Emulators
-rpcs3 #PS3 Emulator
-xemu #Original Xbox Emulator
-ryujinx #Switch Emulator
-retroarchFull #Most Other Emulators
-shadps4 #PS4 I Think
+rpcs3 #PS3.
+shadps4 #PS4.
+xemu #Original Xbox.
+ryujinx #Switch.
+retroarchFull #Most other emulators.
 
 # Studio
-onlyoffice-bin #Office Suite
-davinci-resolve #Video Editor
-gimp #Photo Editor
-inkscape-with-extensions #Illistrator
-blender #3D Model Editor
-audacity #Audio Editor
-obs-studio #OBS
-linuxKernel.packages.linux_6_6.v4l2loopback #OBS Virtual Cam
-kicad
-unityhub
-godot
+onlyoffice-bin #Office suite.
+davinci-resolve #Premeir Pro & After effects.
+gimp #Photoshop.
+inkscape-with-extensions #Illistrator.
+blender #3D model editor.
+audacity #Audio editor.
+obs-studio #OBS.
+linuxKernel.packages.linux_6_6.v4l2loopback #OBS virtual cam.
+kicad #PCB CAD. 
+unityhub #Game engine.
+godot #Other game engine.
 
 # Archive and Compression
-unrar #.rar Files Are Fucking Lame
-p7zip #The GOAT
+unrar #.rar files are fucking lame.
+p7zip #The GOAT.
 
 # Base-Devel
-distrobox
-xorg.xhost
-vim #Based Text Editor
-git #Imagine Not Knowing What Git Is
-git-lfs
-github-desktop #Git For Lazy Fucks
-tmux #Terminal Now Has Autism
-vscode
-dotnet-sdk
+distrobox #Containers instead of vm's.
+xorg.xhost #fml.
+vim #The text editor my grandfather used.
+git #Imagine not knowing what git is.
+github-desktop #Git for lazy fucks.
+git-lfs #Git for fat fucks.
+tmux #Terminal autism.
+vscode #Text editor that gets paid 6 figures.
+dotnet-sdk #C# Things, not a big fan.
+direnv #Load env variables in CLI.
 ];}
