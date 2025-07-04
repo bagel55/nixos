28d429c6f3a7a19677ed97a33baa1521d8290dd6
bagel <bagel2255@protonmail.com>
2025-07-03 19:11:25 -0700

    Auto backup on 2025-07-03 19:11:25

--- driver-configuration-Laptop.nix
+++ driver-configuration-Laptop.nix
@@ -1,35 +1,29 @@
-  { config, lib, pkgs, modulesPath, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
-  boot.kernelParams = [ "intel_pstate=disable" "nvidia.NVreg_OpenRmEnableUnsupportedGpus=1" "nvidia-drm.modeset=1" "nvidia.NVreg_RegistryDwords=PerfLevelSrc=0x2222" ];
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelParams = [
+    "nvidia.NVreg_OpenRmEnableUnsupportedGpus=1"
+    "nvidia-drm.modeset=1"
+    "nvidia.NVreg_RegistryDwords=PerfLevelSrc=0x2222"
+  ];
   boot.blacklistedKernelModules = [ "nouveau" ];
 
-services.xserver = {
-  videoDrivers = [ "nvidia" ];
-  deviceSection = ''
-    Section "Device"
-      Identifier "NVIDIA GPU"
-      Driver "nvidia"
-      BusID "PCI:1:0:0" # Adjust to your NVIDIA GPU PCI bus ID (find with lspci)
-      Option "AllowEmptyInitialConfiguration" "true"
-      Option "PrimaryGPU" "yes"
-    EndSection
-  '';
-};
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+  };
 
-hardware.graphics = {
-  enable = true;
-  enable32Bit = true;
-};
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
-hardware.nvidia = {
-  package = pkgs.linuxPackages.nvidia_x11;
-  modesetting.enable = true;
-  powerManagement.enable = true;
-  powerManagement.finegrained = false;
-  open = false;
-  nvidiaSettings = true;
-};
+  services.xserver.videoDrivers = [ "nvidia" ];
 
-networking.hostName = "Nixos-bagel-Laptop";
+  networking.hostName = "Nixos-bagel-Laptop";
 }
