edf8b38e6582fea71e810f9e76b1d2d3b9134328
bagel <bagel2255@protonmail.com>
2025-07-12 02:24:11 -0700

    Auto backup on 2025-07-12 02:24:11

--- home.nix
+++ home.nix
@@ -1,23 +1,38 @@
 { config, pkgs, ... }: {
-  home.username = "bagel";
-  home.homeDirectory = "/home/bagel";
+home.username = "bagel";
+home.homeDirectory = "/home/bagel";
 
-  programs.home-manager.enable = true;
+programs.home-manager.enable = true;
 
-  programs.zsh = {
+programs.zsh = {
+  enable = true;
+  oh-my-zsh = {
     enable = true;
-    oh-my-zsh = {
-      enable = true;
-      theme = "jonathan";
-      plugins = [ "git" "z" ];
-    };
+    theme = "jonathan";
+    plugins = [ "git" "z" ];
   };
+};
 
-  programs.tmux = {
-    enable = true;
-    clock24 = true;
-    extraConfig = "set -g mouse on";
-  };
+programs.tmux = {
+  enable = true;
+  extraConfig = ''
+    unbind r
+    bind r source-file ~/.tmux.conf
 
-  home.stateVersion = "25.05";
+    set -g prefix C-s
+    set -g mouse on
+
+    set-option -g status-position top
+    set-environment -gu "SSH_ASKPASS"
+
+    # List of plugins
+    set -g @plugin 'tmux-plugins/tpm'
+    set -g @plugin 'janoamaral/tokyo-night-tmux'
+
+    # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+    run '~/.tmux/plugins/tpm/tpm'
+  '';
+};
+
+home.stateVersion = "25.05";
 }
