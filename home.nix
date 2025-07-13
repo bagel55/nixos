16ff9e1842d9b54e3cdd11cfffc99c779e2829d4
bagel <bagel2255@protonmail.com>
2025-07-13 03:53:23 -0700

    Auto backup on 2025-07-13 03:53:23

--- home.nix
+++ home.nix
@@ -39,6 +39,9 @@ programs.tmux = {
     unbind r
     bind r source-file ~/.tmux.conf
 
+    set -g @tokyo-night-tmux_show_path 1
+    set -g @tokyo-night-tmux_path_format relative # 'relative' or 'full'
+
     set -g prefix C-s
     set -g mouse on
 
@@ -49,16 +52,11 @@ programs.tmux = {
     bind - split-window -v -c "#{pane_current_path}"
 
     set -g @tokyo-night-tmux_show_netspeed 1
-set -g @tokyo-night-tmux_netspeed_iface "wlan0" # Detected via default route
-set -g @tokyo-night-tmux_netspeed_showip 1      # Display IPv4 address (default 0)
-set -g @tokyo-night-tmux_netspeed_refresh 1     # Update interval in seconds (default 1)
-
-
-set -g @tokyo-night-tmux_show_path 1
-set -g @tokyo-night-tmux_path_format relative # 'relative' or 'full'
-
-set -g @tokyo-night-tmux_show_hostname 1
-
+    set -g @tokyo-night-tmux_netspeed_iface "wlan0" # Detected via default route
+    set -g @tokyo-night-tmux_netspeed_showip 1      # Display IPv4 address (default 0)
+    set -g @tokyo-night-tmux_netspeed_refresh 1     # Update interval in seconds (default 1)
+    
+    set -g @tokyo-night-tmux_show_hostname 1
   '';
 };
 
