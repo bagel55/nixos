376b8204fae65babf6680e206764103387cf9a9e
bagel <bagel2255@protonmail.com>
2025-07-13 02:25:01 -0700

    Auto backup on 2025-07-13 02:25:01

--- home.nix
+++ home.nix
@@ -3,6 +3,32 @@ let
   repoDirTpm = "${config.home.homeDirectory}/.tmux/plugins/tpm";
   repoDirTkyo = "${config.home.homeDirectory}/.tmux/plugins/tokyo-night-tmux";
   gitPath = "${pkgs.git}/bin/git";
+
+  tmuxConf = ''
+    unbind r
+    bind r source-file ~/.tmux.conf
+
+    set -g prefix C-s
+    set -g mouse on
+
+    set-option -g status-position top
+    set-environment -gu "SSH_ASKPASS"
+
+    # List of plugins
+    set -g @plugin 'tmux-plugins/tpm'
+    set -g @plugin "janoamaral/tokyo-night-tmux"
+
+    # Other examples:
+    # set -g @plugin 'github_username/plugin_name'
+    # set -g @plugin 'github_username/plugin_name#branch'
+    # set -g @plugin 'git@github.com:user/plugin'
+    # set -g @plugin 'git@bitbucket.com:user/plugin'
+
+    # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+    run '~/.tmux/plugins/tpm/tpm'
+  '';
+
+  tmuxConfPath = "${config.home.homeDirectory}/.tmux.conf";
 in 
 {
 home.username = "bagel";
@@ -29,37 +55,16 @@ programs.tmux = {
 };
 
 home.file = {
-  ".tmux.conf".text = ''
-    unbind r
-    bind r source-file ~/.tmux.conf
-
-    set -g prefix C-s
-    set -g mouse on
-
-    set-option -g status-position top
-    set-environment -gu "SSH_ASKPASS"
-
-    # List of plugins
-    set -g @plugin 'tmux-plugins/tpm'
-    set -g @plugin "janoamaral/tokyo-night-tmux"
-
-    # Other examples:
-    # set -g @plugin 'github_username/plugin_name'
-    # set -g @plugin 'github_username/plugin_name#branch'
-    # set -g @plugin 'git@github.com:user/plugin'
-    # set -g @plugin 'git@bitbucket.com:user/plugin'
-
-    # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
-    run '~/.tmux/plugins/tpm/tpm'
-  '';
-};
-
-home.file = {
   ".config/alacritty/alacritty.toml".text = ''
     [window]
     opacity = 0.90
   '';
 };
+
+home.activation.writeTmuxConf = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    echo '${tmuxConf}' > ${tmuxConfPath}
+    #chown ${config.home.username}:${config.home.username} ${tmuxConfPath}
+  '';
 
 home.activation.cloneRepoTpm = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
   if [ ! -d "${repoDirTpm}" ]; then
