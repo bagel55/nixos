096467c7d855c7d6d811cfe6f57b60bc643c4886
bagel <bagel2255@protonmail.com>
2025-07-18 23:48:36 -0700

    Auto backup on 2025-07-18 23:48:36

--- modules/home.nix
+++ modules/home.nix
@@ -68,6 +68,10 @@ programs.home-manager.enable = true;
       opacity = 0.90
       dimensions = { columns = 120, lines = 40 }
 
+      [shell]
+      program = "tmux"
+      args = ["new-session", "-A", "-s", "main"]
+
       [colors.primary]
       background = "#1a1b26"
       foreground = "#c0caf5"
