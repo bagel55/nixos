8eaebf8a848cf4eb495c8f7c5189dcc2c43e8fdc
bagel <bagel2255@protonmail.com>
2025-07-19 07:00:58 -0700

    add file

---
+++ modules/home-containers.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, lib, ... }: {
+home.packages = [ pkgs.tor-browser-bundle-bin ];
+
+  xdg.desktopEntries.tor-browser-container = {
+    name = "Tor Browser (Container)";
+    comment = "Isolated Tor Browser in NixOS container";
+    exec = "/home/bagel/.local/bin/tor-browser-container";
+    icon = "torbrowser";
+    terminal = false;
+    categories = [ "Network" "Privacy" ];
+  };
+
+  home.file.".local/bin/tor-browser-container".text = ''
+    #!/bin/bash
+    xhost +local: > /dev/null
+    sudo nixos-container run tor-browser -- \
+      env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY \
+      sudo -u toruser tor-browser &
+  '';
+
+  home.file.".local/bin/tor-browser-container".executable = true;
+}
