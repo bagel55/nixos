a8c7bcbd8d2b68c3b63f2c86c5cef50cdd077a02
bagel <bagel2255@protonmail.com>
2025-07-19 07:53:47 -0700

    Auto backup on 2025-07-19 07:53:47

--- modules/home-containers.nix
+++ modules/home-containers.nix
@@ -1,36 +1,3 @@
-{ config, pkgs, lib, ... }:
-let
-  uid = builtins.getEnv "UID";
-in {
-  home.packages = [ pkgs.podman ];
-  services.podman.enable = true;
+{ config, pkgs, lib, ... }:{
 
-  services.podman.containers.tor-browser = {
-    image = "docker.io/avpnusr/torbrowser:latest";
-    description = "Tor Browser in Podman";
-    autoStart = true;
-    extraConfig = {
-      Run = {
-        Args = lib.mkForce [
-          "--env" "WAYLAND_DISPLAY"
-          "--env" "XDG_RUNTIME_DIR"
-          "--volume" "/run/user/${toString uid}/wayland-0:/run/user/${toString uid}/wayland-0"
-          "--device" "/dev/dri"
-          "--network" "none"
-          "--read-only"
-          "--tmpfs" "/tmp"
-          "--user" (toString uid)
-          "--name" "tor-browser"
-        ];
-      };
-    };
-  };
-
-  xdg.desktopEntries.tor-browser = {
-    name = "Tor Browser";
-    exec = "podman run --rm --podman-wait tor-browser";
-    icon = "/run/current-system/sw/share/icons/hicolor/128x128/apps/torbrowser.png";
-    terminal = false;
-    categories = [ "Network" "WebBrowser" ];
-  };
 }
