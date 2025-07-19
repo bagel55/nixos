64afbfbb0a2e923a9e8ceb83813d27e8ee8ad3fa
bagel <bagel2255@protonmail.com>
2025-07-19 07:33:04 -0700

    Auto backup on 2025-07-19 07:33:04

--- modules/home-containers.nix
+++ modules/home-containers.nix
@@ -6,7 +6,7 @@ in {
   services.podman.enable = true;
 
   services.podman.containers.tor-browser = {
-    image = "docker.io/parasieu/podman-torbrowser:latest";
+    image = "docker.io/raparicio/torbrowser:latest";
     description = "Tor Browser in Podman";
     autoStart = true;
     extraConfig = {
