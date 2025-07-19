eee7bc58df01181471c5b4b984274688403fd9d9
bagel <bagel2255@protonmail.com>
2025-07-19 08:51:14 -0700

    Auto backup on 2025-07-19 08:51:14

--- modules/containers.nix
+++ modules/containers.nix
@@ -1,12 +1,3 @@
-# nixos/modules/containers.nix
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:{
 
-{
-  # Just enable podman globally; no container definitions here
-  virtualisation.podman.enable = true;
-
-  # Ensure podman rootless works correctly
-  systemd.user.extraConfig = ''
-    DefaultEnvironment="PATH=/run/current-system/sw/bin:/run/wrappers/bin"
-  '';
 }
--- modules/home-containers.nix
+++ modules/home-containers.nix
@@ -1,33 +1,3 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:{
 
-let
-  torImage = "docker.io/domistyle/tor-browser:latest";
-in {
-  services.podman.enable = true;
-
-  systemd.user.services.podman-tor = {
-    Unit = {
-      Description = "Tor Browser (VNC) Container";
-      After = [ "graphical-session.target" ];
-    };
-    Service = {
-      Type = "simple";
-      Restart = "on-failure";
-      ExecStart = "${pkgs.podman}/bin/podman run --rm -p 5800:5800 ${torImage}";
-      ExecStop = "${pkgs.podman}/bin/podman stop tor-browser";
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
-
-  xdg.desktopEntries.tor-browser = {
-    name = "Tor Browser (Podman VNC)";
-    exec = "systemctl --user start podman-tor";
-    icon = "tor-browser";
-    terminal = false;
-    type = "Application";
-    categories = [ "Network" "X-Privacy" ];
-    startupNotify = false;
-  };
 }
