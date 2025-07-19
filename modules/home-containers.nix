90bd0f87108934995b72d227c054a137a3db008d
bagel <bagel2255@protonmail.com>
2025-07-19 08:29:31 -0700

    Auto backup on 2025-07-19 08:29:31

--- modules/home-containers.nix
+++ modules/home-containers.nix
@@ -8,27 +8,28 @@ in {
   services.podman.enable = true;
 
   systemd.user.services.podman-tor = {
-    Unit = {
-      Description = "Tor Browser (Podman Wayland)";
-      After = [ "graphical-session.target" ];
-    };
-    Service = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = ''
-        ${pkgs.podman}/bin/podman run --rm \
-          --userns=keep-id \
-          --ipc=host \
-          -v ${config.home.homeDirectory}/.Xauthority:/home/user/.Xauthority:ro \
-          -v $XDG_RUNTIME_DIR/wayland-0:/run/user/$(id -u)/wayland-0 \
-          -e WAYLAND_DISPLAY=wayland-0 \
-          ${torImage}
-      '';
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
+  Unit = {
+    Description = "Tor Browser (Podman Wayland)";
+    After = [ "graphical-session.target" ];
   };
+  Service = {
+    Type = "oneshot";
+    RemainAfterExit = true;
+    Environment = "XDG_RUNTIME_DIR=/run/user/1000";
+    ExecStart = ''
+      ${pkgs.podman}/bin/podman run --rm \
+        --userns=keep-id \
+        --ipc=host \
+        -v ${config.home.homeDirectory}/.Xauthority:/home/user/.Xauthority:ro \
+        -v $XDG_RUNTIME_DIR/wayland-0:/run/user/$(id -u)/wayland-0 \
+        -e WAYLAND_DISPLAY=wayland-0 \
+        ghcr.io/pariseed/podman-torbrowser:latest
+    '';
+  };
+  Install = {
+    WantedBy = [ "default.target" ];
+  };
+};
 
   xdg.desktopEntries.tor-browser = {
     name = "Tor Browser (Podman)";
