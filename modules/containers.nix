ffe3a688fedb60c68ab1e5cac242db26834d47d3
bagel <bagel2255@protonmail.com>
2025-07-19 08:16:10 -0700

    Auto backup on 2025-07-19 08:16:10

--- modules/containers.nix
+++ modules/containers.nix
@@ -1,3 +1,20 @@
-{ config, pkgs, ... }:{
+{ config, pkgs, lib, ... }:{
+  virtualisation.podman.enable = true;
 
+  virtualisation.oci-containers.containers.tor-browser = {
+    image = "ghcr.io/pariseed/podman-torbrowser:latest";
+    extraOptions = [
+      "--rm"
+      "--ipc=host"
+      "--userns=keep-id"
+    ];
+    volumes = [
+      "/run/user/1000/wayland-0:/run/user/1000/wayland-0"
+      "/home/youruser/.Xauthority:/home/youruser/.Xauthority:ro"
+    ];
+    environment = {
+      WAYLAND_DISPLAY = "wayland-0";
+    };
+    autoStart = false;
+  };
 }
--- modules/home-containers.nix
+++ modules/home-containers.nix
@@ -1,3 +1,10 @@
 { config, pkgs, lib, ... }:{
-
+  xdg.desktopEntries.tor-browser = {
+    name = "Tor Browser (Podman)";
+    exec = "podman start -a tor-browser";
+    icon = "tor-browser"; # optional: path to an icon if needed
+    terminal = false;
+    type = "Application";
+    categories = [ "Network" "X-Privacy" ];
+  };
 }
