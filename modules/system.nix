9252c43dffa42212a07c7f6473ea0a4010265cc1
bagel <bagel2255@protonmail.com>
2025-09-12 15:30:11 -0700

    Auto backup on 2025-09-12 15:30:11

--- modules/system.nix
+++ modules/system.nix
@@ -1,13 +1,6 @@
 { config, pkgs, ... }:{
 # boot
-	boot.loader = {
-    grub = {
-      enable = true;
-      efiSupport = true;
-      device = "nodev";
-      useOSProber = true;
-    };
-  };
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
 # SSD trim
