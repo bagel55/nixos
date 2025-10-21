deddc5df67f74ce3b61a3069e8586e32a1a40f69
bagel <bagel2255@protonmail.com>
2025-10-21 11:16:23 -0700

    Auto backup on 2025-10-21 11:16:23

--- modules/system.nix
+++ modules/system.nix
@@ -19,6 +19,9 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   hardware.enableAllFirmware = true;
 
+# Flatpak
+  services.flatpak.enable = true;
+
 # internationalisation properties
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
