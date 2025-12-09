b18f45c3bfdc0d3ce4218190ff462635b8623b41
bagel <bagel2255@protonmail.com>
2025-12-08 21:06:41 -0800

    Auto backup on 2025-12-08 21:06:41

--- modules/cosmic.nix
+++ modules/cosmic.nix
@@ -2,9 +2,9 @@
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
 
-  #environment.cosmic.excludePackages = with pkgs; [
-  #  cosmic-store
-  #];
+  environment.cosmic.excludePackages = with pkgs; [
+    cosmic-store
+  ];
 
   environment.systemPackages = with pkgs; [
     cosmic-ext-tweaks
