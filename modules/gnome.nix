2d5cd0888570e3ca5e6104f70866b34b901dfe48
bagel <bagel2255@protonmail.com>
2025-12-13 17:19:34 -0800

    Auto backup on 2025-12-13 17:19:34

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,10 +1,9 @@
 { config, pkgs, ... }:{
 # enable gnome
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
+  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm.wayland = true;
+  services.desktopManager.gnome.enable = true;
 # exclude garbage
   services.gnome.core-apps.enable = false;
   environment.gnome.excludePackages = with pkgs; [ pkgs.gnome-tour gnome-backgrounds ];
