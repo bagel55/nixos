c47a34dd0e1f5e282eaaa03ce40857cc017aa7bd
bagel <bagel2255@protonmail.com>
2025-12-13 17:24:27 -0800

    Auto backup on 2025-12-13 17:24:27

--- modules/hosts/desktop/driver-configuration.nix
+++ modules/hosts/desktop/driver-configuration.nix
@@ -10,6 +10,20 @@
 	};
 
 	hardware.amdgpu.opencl.enable = true;
+
+	systemd.tmpfiles.rules =
+    let
+      rocmEnv = pkgs.symlinkJoin {
+        name = "rocm-combined";
+        paths = with pkgs.rocmPackages; [
+          rocblas
+          hipblas
+          clr
+        ];
+      };
+    in [
+      "L+ /opt/rocm - - - - ${rocmEnv}"
+    ];
 	
 	#Hostname
 	networking.hostName = "bagel-desktop-nixos";
