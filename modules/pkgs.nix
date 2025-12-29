987ad0d2c93ac0f7f630de92957c72b0c3fb61e9
bagel <bagel2255@protonmail.com>
2025-12-28 21:40:25 -0800

    Auto backup on 2025-12-28 21:40:25

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -5,12 +5,33 @@
 	  remotePlay.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
     dedicatedServer.openFirewall = true;
-    gamescopeSession.enable = true;
+    gamescopeSession = {
+    enable = true;
+    env = {
+      WLR_RENDERER = "vulkan";
+      DXVK_HDR = "1";
+      ENABLE_GAMESCOPE_WSI = "1";
+      WINE_FULLSCREEN_FSR = "1";
+      SDL_VIDEODRIVER = "x11";
+    };
+    args = [
+      "--xwayland-count 2"
+      "--expose-wayland"
+      "-e"
+      "--steam"
+      "--adaptive-sync"
+      "--hdr-enabled"
+      "--hdr-itm-enable"
+      "--prefer-output HDMI-A-1"
+      "--output-width 2560"
+      "--output-height 1440"
+    ];
+  };
   };
 
   programs.gamescope = {
     enable = true;
-    capSysNice = true; # Allows Gamescope to use real-time scheduling for better performance
+    capSysNice = true;
   };
 
   programs.gamemode = {
