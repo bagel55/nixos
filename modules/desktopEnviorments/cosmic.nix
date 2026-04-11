4e704ee1481683a802d5c8b495e2b96c620912c2
bagel <bagel2255@protonmail.com>
2026-04-11 09:31:05 -0700

    Auto backup on 2026-04-11 09:31:05

--- modules/desktopEnviorments/cosmic.nix
+++ modules/desktopEnviorments/cosmic.nix
@@ -1,14 +1,22 @@
-{ config, pkgs, ... }:{
+{ config, pkgs, ... }: {
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
 
   environment.cosmic.excludePackages = with pkgs; [
     cosmic-store
     cosmic-term
-    networkmanagerapplet
   ];
 
   environment.systemPackages = with pkgs; [
     cosmic-ext-tweaks
+    networkmanagerapplet
+    wl-clipboard
   ];
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  services.dbus.enable = true;
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
 }
