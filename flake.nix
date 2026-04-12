f44a47b15088e56aa4e63b8e420b4d730e123a47
bagel <bagel2255@protonmail.com>
2026-04-12 09:17:37 -0700

    Auto backup on 2026-04-12 09:17:37

--- flake.nix
+++ flake.nix
@@ -1,42 +1,54 @@
 {
-description = "NixOS configuration for bagel";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
-inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-  home-manager.url = "github:nix-community/home-manager/release-25.11";
-  home-manager.inputs.nixpkgs.follows = "nixpkgs";
-};
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-outputs = { self, nixpkgs, home-manager, ... }: {
-  nixosConfigurations = {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }: {
+    nixosConfigurations = {
 
-    bagel-desktop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      bagel-desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
 
-      modules = [
-        ./configuration.nix
-        ./modules/hosts/desktop/hardware-configuration.nix
-        ./modules/hosts/desktop/driver-configuration.nix
+        specialArgs = {
+          unstable = import nixpkgs-unstable {
+            system = "x86_64-linux";
+          };
+        };
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          
-          home-manager.users.bagel = import ./modules/home.nix;
-        }
-      ];
-    };
+        modules = [
+          ./configuration.nix
+          ./modules/hosts/desktop/hardware-configuration.nix
+          ./modules/hosts/desktop/driver-configuration.nix
 
-    bagel-laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
 
-      modules = [
-        ./configuration.nix
-        ./modules/hosts/laptop/hardware-configuration.nix
-        ./modules/hosts/laptop/driver-configuration.nix
-      ];
+            home-manager.users.bagel = import ./modules/home.nix;
+          }
+        ];
+      };
+
+      bagel-laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        specialArgs = {
+          unstable = import nixpkgs-unstable {
+            system = "x86_64-linux";
+          };
+        };
+
+        modules = [
+          ./configuration.nix
+          ./modules/hosts/laptop/hardware-configuration.nix
+          ./modules/hosts/laptop/driver-configuration.nix
+        ];
+      };
     };
   };
-};
 }
