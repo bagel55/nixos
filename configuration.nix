1eaa91ae2f4e038b337da94a0cb926aa2908051d
bagel <bagel2255@protonmail.com>
2026-05-16 11:52:52 -0700

    Auto backup on 2026-05-16 11:52:52

--- configuration.nix
+++ configuration.nix
@@ -4,9 +4,8 @@ imports =[
   ./modules/env.nix
   ./modules/desktopEnviorments/gnome.nix
   #./modules/desktopEnviorments/cosmic.nix
-  #./modules/desktopEnviorments/plasma.nix
   ./modules/applications/unity.nix
   ./modules/automation/git-ops.nix
   ./modules/automation/activation-scripts.nix
 ];
-system.stateVersion = "24.05";}
+system.stateVersion = "25.11";}
