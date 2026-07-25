7745152f958520da5386791081b3ab9c98486d6b
bagel <bagel2255@protonmail.com>
2026-07-25 11:50:01 -0700

    Auto backup on 2026-07-25 11:50:01

--- flake.nix
+++ flake.nix
@@ -3,11 +3,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
+    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
+    nixos-cosmic.inputs.nixpkgs.follows = "nixpkgs";
+
     home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-cosmic, home-manager, ... }: {
     nixosConfigurations = {
 
       bagel-desktop = nixpkgs.lib.nixosSystem {
@@ -23,6 +26,14 @@
           ./configuration.nix
           ./modules/hosts/desktop/hardware-configuration.nix
           ./modules/hosts/desktop/driver-configuration.nix
+
+          {
+            nix.settings = {
+              substituters = [ "https://cosmic.cachix.org/" ];
+              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+            };
+          }
+          nixos-cosmic.nixosModules.default
 
           home-manager.nixosModules.home-manager
           {
--- modules/desktopEnviorments/cosmic.nix
+++ modules/desktopEnviorments/cosmic.nix
@@ -1,22 +1,22 @@
 { config, pkgs, ... }: {
-  services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
 
+  services.desktopManager.cosmic.xwayland.enable = true;
+
+  # Exclude packages you don’t want
   environment.cosmic.excludePackages = with pkgs; [
-    cosmic-store
-    cosmic-term
+    # Examples of things people commonly remove:
+    # cosmic-edit          # text editor
+    # cosmic-files         # file manager (careful – can break session if removed alone)
+    # cosmic-term          # terminal
+    # cosmic-player        # media player
+    # cosmic-store         # app store (only useful if you use Flatpak)
+    # cosmic-wallpapers
   ];
 
+  # Extra packages you actually want
   environment.systemPackages = with pkgs; [
-    cosmic-ext-tweaks
-    networkmanagerapplet
-    wl-clipboard
+    cosmic-screenshot
   ];
-
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
-  services.dbus.enable = true;
-  services.gvfs.enable = true;
-  services.udisks2.enable = true;
 }
