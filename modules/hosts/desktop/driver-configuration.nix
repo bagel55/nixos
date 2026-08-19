85aa5e7c37542cf2155cc85d15f1cf8f61e978bb
bagel <bagel2255@protonmail.com>
2026-08-18 17:37:12 -0700

    Auto backup on 2026-08-18 17:37:12

--- modules/hosts/desktop/driver-configuration.nix
+++ modules/hosts/desktop/driver-configuration.nix
@@ -11,46 +11,6 @@
     package32 = unstable.pkgsi686Linux.mesa;
 	};
 
-	hardware.amdgpu.opencl.enable = true;
-
-  ################################################
-  #########AHHHHHHHHHHHHHHHHHHHHHHHHHHH###########
-  ################################################
-  hardware.graphics.extraPackages = with pkgs; [
-    rocmPackages.rocm-runtime
-    rocmPackages.rocm-device-libs
-    rocmPackages.rocm-smi
-  ];
-
-  environment.systemPackages = with pkgs; [
-    rocmPackages.rocminfo
-    rocmPackages.rocm-smi
-    pciutils
-  ];
-
-  # Required for RDNA3
-  environment.variables = {
-    HSA_OVERRIDE_GFX_VERSION = "11.0.0";
-  };
-
-	#stuff for ROCm for stable diff
-	systemd.tmpfiles.rules =
-    let
-      rocmEnv = pkgs.symlinkJoin {
-        name = "rocm-combined";
-        paths = with pkgs.rocmPackages; [
-          rocblas
-          hipblas
-          clr
-        ];
-      };
-    in [
-      "L+ /opt/rocm - - - - ${rocmEnv}"
-    ];
-	################################################
-  #########AHHHHHHHHHHHHHHHHHHHHHHHHHHH###########
-  ################################################
-
 	#Hostname
 	networking.hostName = "bagel-desktop-nixos";
 }
